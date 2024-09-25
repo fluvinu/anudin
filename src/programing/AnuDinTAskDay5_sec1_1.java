95cd04954d39d1ae7fbb8eac66282fcfa2071572
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-09-25 06:10:57 +0000

    arrypro

---
+++ src/programing/AnuDinTAskDay5_sec1_1.java
@@ -0,0 +1,11 @@
+package programing;
+
+class AnuDinTAskDay5_sec1_1{
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,5,6};
+        
+        for(int i=0;i<=arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
