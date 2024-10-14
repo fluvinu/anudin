6adde39a56529f3373302f4039b17fc66f04ec7d
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-10-14 22:19:41 +0530

    inial commant

---
+++ jdbcEX/src/main/java/com/jdbc/jdbcEX/JdbcExApplication.java
@@ -0,0 +1,13 @@
+package com.jdbc.jdbcEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcExApplication.class, args);
+	}
+
+}
---
+++ jdbcEX/src/test/java/com/jdbc/jdbcEX/JdbcExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.jdbcEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
