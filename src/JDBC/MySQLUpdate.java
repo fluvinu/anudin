0f9fb85c3d437e154d359b96753c75bd7d3fb494
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-10-15 18:40:55 +0000

    update

--- src/JDBC/MySQLUpdate.java
+++ src/JDBC/MySQLUpdate.java
@@ -11,15 +11,25 @@ public class MySQLUpdate{
         String user = "avnadmin";
         String password = "AVNS_p-pByAc3oT8SAs5Uh6W";
 
-        String strQ="";
-
+        String strQ="update students set first_name=?, last_name=?, date_of_birth=?, grade=? where student_id=? ;";
+        String stuId="2";
         try( Connection con=DriverManager.getConnection(url,user,password);
         PreparedStatement pstmt=con.prepareStatement(strQ)){
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
+            pstmt.setString(1, "Siddharth");
+            pstmt.setString(2, "Shivwanshi");
+            pstmt.setString(3, "2001-01-2");
+            pstmt.setString(4, "A");
+            pstmt.setString(5, stuId);
+
+            int executeUpdate = pstmt.executeUpdate();
+
+            System.out.println(executeUpdate+" row updated ");
+
         }catch (Exception e) {
             // TODO: handle exception
-
+            System.out.println(e);
         }
     }
+
 }
