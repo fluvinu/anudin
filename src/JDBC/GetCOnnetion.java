e073e2c61334403d1bab46d18561a4574995f5cc
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-10-15 20:29:25 +0000

    new

---
+++ src/JDBC/GetCOnnetion.java
@@ -0,0 +1,26 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class GetCOnnetion {
+    static Connection con;
+    static String url="jdbc:mysql://mysql-1eafdc82-siddharthshivwanshi-800b.e.aivencloud.com:16981/anudin";
+    static String user="avnadmin";
+    static String password="AVNS_p-pByAc3oT8SAs5Uh6W";
+
+    static Connection getcon(){
+        if(con==null){
+            GetCOnnetion.makeCon();
+        }
+        return con;
+    }
+
+    static void makeCon(){
+        try {
+            con= DriverManager.getConnection(url,user,password);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
--- src/JDBC/MySqlExample.java
+++ src/JDBC/MySqlExample.java
@@ -1,19 +1,13 @@
 package JDBC;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MySqlExample {
     public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://mysql-1eafdc82-siddharthshivwanshi-800b.e.aivencloud.com:16981/anudin";
-        String user = "avnadmin";
-        String password = "AVNS_p-pByAc3oT8SAs5Uh6W";
 
-        try (final Connection connection = DriverManager.getConnection(url, user, password);
+        try (final Connection connection = GetCOnnetion.getcon();
              final Statement statement = connection.createStatement();
              final ResultSet resultSet = statement.executeQuery("SELECT version() AS version")) {
 
